421e8036adf69933c2ea2df182a9ec4c8e4a3c7c
itsprinceAiMl <princekdigitalvibe@gmail.com>
2025-06-24 17:25:20 +0530

    nhk

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('.reservation-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert("Thank you! Your reservation has been submitted.");
+  });
+  
